7467bf6f6bc95e8e0ffd770fe3927fe13eb4dabc
Nicole Kaye <nkrodriguez.bilon@gmail.com>
2021-05-23 10:56:53 +0800

    added cors to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const cropRouter = require('./routers/crop')
@@ -8,6 +9,7 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.use(cropRouter)
 
