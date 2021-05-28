e9e69860a9b4e0650e332b95bf5e0c635b82f4c4
Nicole Kaye <nkrodriguez.bilon@gmail.com>
2021-05-28 21:16:08 +0800

    temporarily commented out jwt expiresIn option

--- src/models/user.js
+++ src/models/user.js
@@ -79,8 +79,9 @@ userSchema.statics.checkPassword = async (password) => {
  */
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET, {expiresIn: '15 minutes'})
-
+    // temporarily removing expiration for API integration testing
+    // const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET, {expiresIn: '15 minutes'})
+    const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({token})
     await user.save()
 
